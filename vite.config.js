12415e555d24b69b34b93be212d20841e782602b
alonageorge <alonageorge2510@gmail.com>
2024-10-14 12:02:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   
   plugins: [react()],
   test: {
-    globals: true, // Enable globals like describe and test
+    globals: true,
+    environment: 'jsdom',// Enable globals like describe and test
      // Set up a jsdom environment for testing
   },
 })
